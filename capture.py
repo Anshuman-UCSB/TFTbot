aaa5d5f20e0a06f3de4d4e8a93c4cf10da4fa4f4
Anshuman Dash <anshuman@ucsb.edu>
2022-04-06 15:21:50 -0700

    :)

--- capture.py
+++ capture.py
@@ -8,16 +8,27 @@ import pickle
 from imageUtil import *
 
 win = pygetwindow.getWindowsWithTitle("BlueStacks App")[0]
-win.moveTo(0,0)
 
 try:
 	champValues = pickle.load(open('champValues.pkl','rb'))
 except FileNotFoundError:
 	champValues = {}
 
+storePos = [
+	(131, 48),
+	(305, 48),
+	(479, 48),
+	(653, 48),
+	(828, 48),
+]
+
+cardSize = (298-131, 249-48)
+
+i = 0
 while True:
 	l,t,r,b = (win.left, win.top, win.right, win.bottom)
 	win.resizeTo(1040,600)
+	# win.moveTo(0,0)
 	# win.height = 600
 	if(r-l != 1040):
 		print(win)
@@ -25,6 +36,8 @@ while True:
 		continue
 	sc = screenshot(l, t, r, b)
 	cv.imshow('capture', sc)
+	i = (i+1)%5
+	cv.imshow("store1", sc[storePos[i][1]:storePos[i][1]+cardSize[1],storePos[i][0]:storePos[i][0]+cardSize[0]])
 	if cv.waitKey(1) == ord('q'):
 		cv.destroyAllWindows()
 		break
